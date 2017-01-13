aa26e1cf65771aef38eae2f63b031eabf17a8bd3
gastikirs <gaston.kirsman@gmail.com>
2017-01-13 09:39:45 +0200

    Update middleware for localhost

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const PORT = process.env.PORT || 3000; //Me lo va a dar Heroku. Por default si n
 //Redirigir el trafico https al http
 
 app.use(function (req, res, next) {
-	if (req.headers['x-forwarded-proto'] == 'http') {
-		next();
-	} else {
+	if (req.headers['x-forwarded-proto'] == 'https') {
 		res.redirect('http://' + req.hostname + req.url);
+
+	} else {
+			next();
 	}
 });
 
